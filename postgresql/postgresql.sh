6cf92ed7e646b6bb277e47ecff8869a1abb4f76a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2023-06-05 13:15:39 +0000

    postgresql: add dump/load commands

--- postgresql/postgresql.sh
+++ postgresql/postgresql.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 set -euo pipefail
 
-vol=/mnt/bbguimaraes1-vol/postgresql
-exec podman run \
-    --name postgresql \
-    --replace \
-    --detach \
-    --user 1000180000:users \
-    --network nextcloud \
-    --read-only \
-    --tmpfs /run/postgresql \
-    --volume "$vol:/var/lib/postgresql:Z" \
-    postgresql
+main() {
+    [[ "$#" -eq 0 ]] && { start; return }
+    local cmd=$1; shift
+    case "$cmd" in
+    dumpall) dumpall "$@";;
+    load) load "$@";;
+    esac
+}
+
+usage() {
+    cat >&2 <<EOF
+Usage: $0 [dumpall|load]
+EOF
+    return 1
+}
+
+start() {
+    local vol=/mnt/bbguimaraes1-vol/postgresql
+    exec podman run \
+        --name postgresql \
+        --replace \
+        --detach \
+        --user 1000180000:users \
+        --network nextcloud \
+        --read-only \
+        --tmpfs /run/postgresql \
+        --volume "$vol:/var/lib/postgresql:Z" \
+        postgresql
+}
+
+dumpall() {
+    exec podman exec postgresql /usr/local/bin/wrapper.sh pg_dumpall
+}
+
+load() {
+    exec podman exec --interactive postgresql /usr/local/bin/wrapper.sh psql
+}
+
+main "$@"
