088093d38c3d8fa1449fb4aa1e215443eea525ea
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2023-06-12 18:12:55 +0000

    synapse: add command processing to script

--- synapse/synapse.sh
+++ synapse/synapse.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
-vol=/mnt/bbguimaraes1-vol/synapse
-exec podman run \
-    --name synapse \
-    --replace \
-    --detach \
-    --user 1000100000:users \
-    --network synapse \
-    --read-only \
-    --volume "$vol:/var/lib/synapse:Z" \
-    synapse
+main() {
+    [[ "$#" -eq 0 ]] && { cmd_start; return; }
+    local cmd=$1; shift
+    case "$cmd" in
+    *) usage;;
+    esac
+}
+
+usage() {
+    cat <<EOF
+Usage: $0 [CMD]
+EOF
+    return 1
+}
+
+cmd_start() {
+    local vol=/mnt/bbguimaraes1-vol/synapse
+    exec podman run \
+        --name synapse \
+        --replace \
+        --detach \
+        --user 1000100000:users \
+        --network static \
+        --read-only \
+        --volume "$vol:/var/lib/synapse:Z" \
+        synapse
+}
+
+main "$@"
