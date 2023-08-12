4868fe447c3e0a905416376fd1987194a4481938
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2023-08-12 10:28:40 +0200

    gitlab: add upgrade command

--- gitlab/gitlab.sh
+++ gitlab/gitlab.sh
@@ -6,6 +6,7 @@ main() {
     local cmd=$1; shift
     case "$cmd" in
     bundle) bundle "$@";;
+    upgrade) upgrade "$@";;
     *) usage;;
     esac
 }
@@ -17,12 +18,18 @@ Usage: $0 CMD [ARGS...]
 Commands:
 
     bundle ARGS...
+    upgrade
 EOF
     return 1
 }
 
 bundle() {
     exec podman exec gitlab-puma bundle-2.7 "$@"
+}
+
+upgrade() {
+    bundle exec rake db:migrate:status RAILS_ENV=production
+    bundle exec rake db:migrate RAILS_ENV=production
 }
 
 main "$@"
