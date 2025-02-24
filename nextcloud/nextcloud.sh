2bf2c7919c4417417c74960b20f2d1b90175aade
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-02-24 17:38:33 +0100

    Add logs commands

--- nextcloud/nextcloud.sh
+++ nextcloud/nextcloud.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+DIR=/usr/share/webapps/nextcloud
+
 main() {
     [[ "$#" -eq 0 ]] && usage
     local cmd=$1; shift
     case "$cmd" in
     cron) cmd cron.php "$@";;
+    logs) logs "$@";;
     occ) cmd occ "$@";;
     upgrade) upgrade;;
     *) usage;;
@@ -19,6 +22,7 @@ Usage: $0 CMD ARGS...
 Commands:
 
     cron ARGS...
+    logs
     occ ARGS...
 EOF
     return 1
@@ -27,13 +31,17 @@ EOF
 cmd() {
     local cmd=$1; shift
     exec podman exec nextcloud-php \
-        php-legacy "/usr/share/webapps/nextcloud/$cmd" "$@"
+        php-legacy "$DIR/$cmd" "$@"
 }
 
 upgrade() {
     cmd occ upgrade
     cmd occ db:add-missing-indices
     cmd occ maintenance:repair --include-expensive
+}
+
+logs() {
+    podman exec nextcloud-php cat "$DIR/data/nextcloud.log"
 }
 
 main "$@"
